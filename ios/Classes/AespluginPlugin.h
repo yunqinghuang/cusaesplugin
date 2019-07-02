27ac08671ef5d86fced214826a62a76f0826274b
yunqinghuang <52435206+yunqinghuang@users.noreply.github.com>
2019-07-02 11:50:01 +0800

    Add files via upload

---
+++ ios/Classes/AespluginPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface AespluginPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/AespluginPlugin.m
@@ -0,0 +1,20 @@
+#import "AespluginPlugin.h"
+
+@implementation AespluginPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"aesplugin"
+            binaryMessenger:[registrar messenger]];
+  AespluginPlugin* instance = [[AespluginPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
